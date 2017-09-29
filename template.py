527972944a9d4bb09d1f72aadfe7597bbf2c3ea4
Dantata <dantata@dantata1.com>
2017-09-29 05:43:09 +0300

    y

--- template.py
+++ template.py
@@ -2,29 +2,31 @@
 # todo: 
 #	change the tkinter import not to use *
 #	clean up the code
-#	decide whether to close the gui after making the selection and pasting the text
+# 	somehow make a nested optionmenu in order to implement /abuse templates from separate directory
 
 import pyautogui
 import os
 from tkinter import *
 
 # setting up the templates directory and obtaining a list of the files in it
-destdir = '/home/dantata/python/template101/templates/'
+destdir = '/home/dantata/python/template101/template101/templates/'
+
 files = [ f for f in os.listdir(destdir) if os.path.isfile(os.path.join(destdir,f)) ]
 
 master = Tk()
 
 # geting the selection from the optionsmenu and pasting the text into the active window
 def paste_selection(value):
-    #print(destdir + value)
+    
     f = open(destdir + value, 'r')
     file_contents = f.read()
 
+	# ALT+TAB in order to return to textarea and then paste the text from the template txt file
     pyautogui.hotkey('alt','tab')
     pyautogui.typewrite(file_contents)
 
     f.close()
-    #sys.exit()
+    sys.exit()
 
 
 variable = StringVar(master)
@@ -32,6 +34,5 @@ variable.set("Click me!") # default value
 
 w = OptionMenu(master, variable, *files,command=paste_selection)
 w.pack()
-
 
 mainloop()
